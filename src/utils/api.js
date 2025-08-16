ce00634453a10de7a8ca27474d7fb7b0269868d0
lbandku <github_username@users.noreply.github.com>
2025-08-15 21:19:10 -0400

    Set default weather location and load on inital render

---
+++ src/utils/api.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+//fetch geocoords from PositionStack API
+export async function fetchCoordinates(location){
+    const response=await axios.get('http://api.positionstack.com/v1/forward',{
+        params:{
+            access_key: import.meta.env.VITE_GEOCODE_API_KEY,
+            query: location,
+            limit: 1
+        }
+});
+
+const{latitude,longitude}=response.data.data[0];
+return{lat:latitude, lon:longitude};
+}
+
+
+//fetch weather from OpenWeather API
+export async function fetchWeather(lat,lon){
+    const response=await axios.get('https://api.openweathermap.org/data/2.5/weather',{
+        params:{
+            lat,
+            lon,
+            units:'imperial',
+            appid:import.meta.env.VITE_WEATHER_API_KEY
+        }
+    });
+    return response.data;
+}
+
